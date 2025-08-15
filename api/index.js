e64f90e5bc58e7a167ff18bbbae3f1117c7fab13
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-15 22:23:49 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // api/index.js - Add 1 user to Google DB
 export default async function handler(req, res) {
   try {
-    await fetch('https://script.google.com/macros/s/AKfycbyqVvgb43Xv_wNbUXZygfj-0wEa0ZV-k2h3ZyrWEzW2x3YWaABxJ9PwZ7-CmH1moKp0/exec');
+    await fetch('https://script.google.com/macros/s/AKfycbytauXkBFmKXVxY514_y7raBTEKr32MxfyX7M1mNmtF9vuXfbDem08wVZYNcvriBhE4/exec');
     res.status(200).end();
   } catch (error) {
     console.error('Error fetching Google Script:', error);

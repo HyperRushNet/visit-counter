1a29919a8d8445c42630574442fc57a226ec459b
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-15 23:44:26 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,10 @@
-// /api/index.js
 export default async function handler(req, res) {
   const scriptUrl = "https://script.google.com/macros/s/AKfycbzVpymE_nsWEDkI65Rf287YxLU2g0j_xXHV3tTjWlc2qPcmylR3qxBct5MaauHUeUGy/exec";
 
-  // Zet CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*'); // alle origins toegestaan
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, User-Agent');
 
-  // OPTIONS request voor preflight afhandelen
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
@@ -18,9 +15,9 @@ export default async function handler(req, res) {
         'User-Agent': 'vercel-fetch'
       }
     });
-    res.status(200).json({ message: 'Teller verhoogd!' });
+    res.status(200).json({ message: '+1 - Counter' });
   } catch (error) {
     console.error('Google Script fetch error:', error);
-    res.status(500).json({ error: 'Kan Google Script niet bereiken' });
+    res.status(500).json({ error: 'Cannot reach Google Script.' });
   }
 }

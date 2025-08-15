5d4bce9c2694f080c726e3dd2a10dcd5173bd1fa
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-16 00:13:38 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,25 @@
+// File: /api/undex.js
 export default function handler(req, res) {
   const scriptUrl = "https://script.google.com/macros/s/AKfycbzVpymE_nsWEDkI65Rf287YxLU2g0j_xXHV3tTjWlc2qPcmylR3qxBct5MaauHUeUGy/exec";
 
-  // CORS headers
+  // CORS headers voor browser fetch
   res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, User-Agent');
 
+  // OPTIONS preflight
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
 
-  // **Direct response naar user**
+  // **Direct antwoord aan de client**
   res.status(200).json({ message: '+1 scheduled - Counter' });
 
-  // **Background fetch zonder await**
+  // **Background fetch fire-and-forget**
   (async () => {
     try {
+      // voorkomen dat meerdere fetches tegelijk lopen
       global._pendingFetch = global._pendingFetch || false;
-
       if (!global._pendingFetch) {
         global._pendingFetch = true;
 

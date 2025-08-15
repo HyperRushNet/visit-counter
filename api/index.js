c1e9cfb3f01827dc4f4a77fe84cd6833e229c9a4
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-15 23:39:54 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,17 @@
 export default async function handler(req, res) {
   const scriptUrl = "https://script.google.com/macros/s/AKfycbzVpymE_nsWEDkI65Rf287YxLU2g0j_xXHV3tTjWlc2qPcmylR3qxBct5MaauHUeUGy/exec";
 
+  // Zet CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*'); // alle origins toegestaan
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, User-Agent');
+
+  // OPTIONS request voor preflight afhandelen
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   try {
-    // Stuur User-Agent mee zodat Google Script weet dat het een fetch is
     await fetch(`${scriptUrl}?ua=vercel-fetch`, {
       headers: {
         'User-Agent': 'vercel-fetch'
